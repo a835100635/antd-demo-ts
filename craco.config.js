4f1b60df072f77df70ffbaa93ffa8b932dea4946
leonli <leonli@wezhuiyi.com>
2021-10-19 10:41:43 +0800

    first commit

---
+++ craco.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  webpack: {
+      configure: {
+          module: {
+              rules: [
+                  {
+                      test: /\.mjs$/,
+                      include: /node_modules/,
+                      type: "javascript/auto"
+                  }
+              ],
+          }
+      },
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactEditor from './pages/reactEditor';
+import { Modal, Button } from 'antd';
 
 function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button type="primary" onClick={showModal}>
+          Open Modal
+        </Button>
+        <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+          <ReactEditor />
+        </Modal>
       </header>
     </div>
   );
