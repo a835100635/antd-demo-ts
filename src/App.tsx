cc0908426c833e3fe0d1be94fb04ef6072711b98
leonli <leonli@wezhuiyi.com>
2021-10-19 15:26:33 +0800

    bugfix: editor销毁时的错误

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,43 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ReactEditor from './pages/reactEditor';
-import { Modal, Button } from 'antd';
+import { useState, useRef } from "react";
+import "./App.css";
+import ReactEditor from "./pages/reactEditor";
+import { Modal, Button } from "antd";
 
 function App() {
+  const pchildref: any = useRef({});
+
   const [isModalVisible, setIsModalVisible] = useState(false);
 
   const showModal = () => {
     setIsModalVisible(true);
   };
 
+  // model确认前  执行editor销毁事件
   const handleOk = () => {
+    pchildref.current.destroyAction();
     setIsModalVisible(false);
   };
 
+  // model关闭前 执行editor销毁事件
   const handleCancel = () => {
+    pchildref.current.destroyAction();
     setIsModalVisible(false);
   };
+
   return (
     <div className="App">
       <header className="App-header">
         <Button type="primary" onClick={showModal}>
           Open Modal
         </Button>
-        <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-          <ReactEditor />
+        <Modal
+          title="Basic Modal"
+          visible={isModalVisible}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          destroyOnClose={true}
+        >
+          <ReactEditor wrapRef={pchildref} />
         </Modal>
       </header>
     </div>
--- src/pages/reactEditor.tsx
+++ src/pages/reactEditor.tsx
@@ -1,62 +1,85 @@
-import React, { useState, useEffect } from 'react'
-import '@wangeditor/editor/dist/css/style.css'
-import { IDomEditor, IEditorConfig, IToolbarConfig, SlateDescendant } from '@wangeditor/editor'
-import { Editor, Toolbar } from '@wangeditor/editor-for-react'
+import React, { useState, useEffect, useImperativeHandle } from "react";
+import "@wangeditor/editor/dist/css/style.css";
+import {
+  IDomEditor,
+  IEditorConfig,
+  IToolbarConfig,
+  SlateDescendant,
+} from "@wangeditor/editor";
+import { Editor, Toolbar } from "@wangeditor/editor-for-react";
 
-function ReactEditor() {
-    // 存储 editor 实例
-    const [editor, setEditor] = useState<IDomEditor | null>(null)
-    // 存储 editor 的最新内容（json 格式）
-    const [curContent, setCurContent] = useState<SlateDescendant[]>([])
+function ReactEditor(props: any) {
+  const { wrapRef } = props;
+  // 存储 editor 实例
+  const [editor, setEditor] = useState<IDomEditor | null>(null);
+  // 存储 editor 的最新内容（json 格式）
+  const [curContent, setCurContent] = useState<SlateDescendant[]>([]);
 
-    // 工具栏配置
-    const toolbarConfig: Partial<IToolbarConfig> = { /* 工具栏配置 */ }
+  // 工具栏配置
+  const toolbarConfig: Partial<IToolbarConfig> = {
+    /* 工具栏配置 */
+  };
 
-    // editor 配置
-    const editorConfig: Partial<IEditorConfig> = {}
-    editorConfig.placeholder = '请输入内容...'
-    editorConfig.onCreated = (editor: IDomEditor) => {
-        // 记录 editor 实例，重要 ！
-        // 有了 editor 实例，就可以执行 editor API
-        setEditor(editor)
-    }
-    editorConfig.onChange = (editor: IDomEditor) => {
-        // editor 选区或者内容变化时，获取当前最新的的 content
-        setCurContent(editor.children)
-    }
-    // 其他编辑器配置
-    // 菜单配置
+  // editor 配置
+  const editorConfig: Partial<IEditorConfig> = {};
+  editorConfig.placeholder = "请输入内容...";
+  editorConfig.onCreated = (editor: IDomEditor) => {
+    // 记录 editor 实例，重要 ！
+    // 有了 editor 实例，就可以执行 editor API
+    setEditor(editor);
+  };
+  editorConfig.onChange = (editor: IDomEditor) => {
+    // editor 选区或者内容变化时，获取当前最新的的 content
+    setCurContent(editor.children);
+  };
+  // 其他编辑器配置
+  // 菜单配置
 
-    // 及时销毁 editor ，重要！！！
-    useEffect(() => {
-        return () => {
-            if (editor == null) return
-            editor.destroy()
-            setEditor(null)
+  // 及时销毁 editor ，重要！！！
+  useEffect(() => {
+    return () => {
+      if (editor == null) return;
+      editor.destroy();
+      setEditor(null);
+    };
+  }, []);
+
+  useImperativeHandle(wrapRef, () => {
+    return {
+      destroyAction() {
+        if (editor) {
+          console.log("销毁开始-->", editor);
+          editor.destroy();
+          setEditor(null);
+          console.log("销毁完成-->", editor);
         }
-    }, [])
+      },
+    };
+  });
 
-    return (
+  return (
+    <div>
         <React.Fragment>
-            <div style={{ border: '1px solid #ccc'}}>
-                {/* 渲染 toolbar */}
-                <Toolbar
-                    editor={editor}
-                    defaultConfig={toolbarConfig}
-                    mode="default"
-                    style={{ borderBottom: '1px solid #ccc' }}
-                />
+          <div style={{ border: "1px solid #ccc" }}>
+            {/* 渲染 toolbar */}
+            <Toolbar
+              editor={editor}
+              defaultConfig={toolbarConfig}
+              mode="default"
+              style={{ borderBottom: "1px solid #ccc" }}
+            />
 
-                {/* 渲染 editor */}
-                <Editor
-                    defaultConfig={editorConfig}
-                    defaultContent={[]}
-                    mode="default"
-                    style={{ height: '500px' }}
-                />
-            </div>
+            {/* 渲染 editor */}
+            <Editor
+              defaultConfig={editorConfig}
+              defaultContent={[]}
+              mode="default"
+              style={{ height: "400px" }}
+            />
+          </div>
         </React.Fragment>
-    )
+    </div>
+  );
 }
 
-export default ReactEditor
+export default ReactEditor;
